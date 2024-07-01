55f1a3a862ce2edf22df506178996478e2a6cf6d
claudiooleite <152962012+claudiooleite@users.noreply.github.com>
2024-07-01 11:26:06 +0200

    scrolltotop component added

--- src/App.js
+++ src/App.js
@@ -1,15 +1,29 @@
-import { Routes, Route } from "react-router-dom";
+import { useEffect } from "react";
+import { Routes, Route, useLocation } from "react-router-dom";
 import Home from "./pages/Home";
 import WrongWay from "./pages/WrongPage";
 import ShopProduct from "./pages/Shop-Product";
 
+function ScrollToTop() {
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
+
+  return null;
+}
+
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/product/:productName" element={<ShopProduct />} />
-      <Route path="*" element={<WrongWay />} />
-    </Routes>
+    <>
+      <ScrollToTop />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/product/:productName" element={<ShopProduct />} />
+        <Route path="*" element={<WrongWay />} />
+      </Routes>
+    </>
   );
 }
 
