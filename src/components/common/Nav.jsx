e35f7d820e4115a75ea709628612010f53bfc5dd
claudiooleite <152962012+claudiooleite@users.noreply.github.com>
2024-07-02 21:40:58 +0200

    imagelogo added to file

--- src/components/common/Nav.jsx
+++ src/components/common/Nav.jsx
@@ -1,25 +1,33 @@
-import { useContext } from "react";
+import React, { useContext } from "react";
 import { Link } from "react-router-dom";
 import '../../assets/styles/nav.css';
 import BasketContext from "../../hooks/basketContext";
+import Image1 from "../../assets/images/logo2.png";
 
 const Nav = ({ onOpen }) => {
   const { basket } = useContext(BasketContext);
+
   return (
     <div className="nav-position-fixed">
       <nav className="nav-parent">
-        <Link className="nav-logo" to="/">Home</Link>
+        <Link className="nav-logo" to="/">
+          <img src={Image1}
+          />
+          <h1>Music Store</h1>
+        </Link>
         <ul className="nav-links">
-          <Link to="/howitworks">How It works</Link>
-          <Link to="/workflow">Workflows</Link>
-          <Link to="/download">Download</Link>
-          <Link to="/blog">Blog</Link>
-          <Link to="/support">Support</Link>
-          <Link to="/shop">Shop</Link>
-          <button onClick={onOpen} className="basket-button">
-            Basket: {basket.length}
-          </button>
-          <Link to="/cleng">Leng</Link>
+          <li><Link to="/howitworks">How It Works</Link></li>
+          <li><Link to="/workflow">Workflows</Link></li>
+          <li><Link to="/download">Download</Link></li>
+          <li><Link to="/blog">Blog</Link></li>
+          <li><Link to="/support">Support</Link></li>
+          <li><Link to="/shop">Shop</Link></li>
+          <li>
+            <button onClick={onOpen} className="basket-button">
+              Basket: {basket.length}
+            </button>
+          </li>
+          <li><Link to="/cleng">Leng</Link></li>
         </ul>
       </nav>
     </div>
