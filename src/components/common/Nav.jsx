d54a54578e10b3342c50db63492e05718d468482
claudiooleite <152962012+claudiooleite@users.noreply.github.com>
2024-07-03 10:14:42 +0200

    navbar ubdated to show items number

--- src/components/common/Nav.jsx
+++ src/components/common/Nav.jsx
@@ -1,5 +1,7 @@
 import React, { useContext } from "react";
 import { Link } from "react-router-dom";
+import { Box, Badge, IconButton } from "@chakra-ui/react";
+import { FiShoppingBag } from "react-icons/fi"; // Import shopping cart icon
 import '../../assets/styles/nav.css';
 import BasketContext from "../../hooks/basketContext";
 import Image1 from "../../assets/images/logo2.png";
@@ -11,8 +13,7 @@ const Nav = ({ onOpen }) => {
     <div className="nav-position-fixed">
       <nav className="nav-parent">
         <Link className="nav-logo" to="/">
-          <img src={Image1}
-          />
+          <img src={Image1} alt="Music Store Logo" />
           <h1>Music Store</h1>
         </Link>
         <ul className="nav-links">
@@ -22,10 +23,22 @@ const Nav = ({ onOpen }) => {
           <li><Link to="/blog">Blog</Link></li>
           <li><Link to="/support">Support</Link></li>
           <li><Link to="/shop">Shop</Link></li>
-          <li>
-            <button onClick={onOpen} className="basket-button">
-              Basket: {basket.length}
-            </button>
+          <li className="basket-container">
+            <IconButton
+              icon={<FiShoppingBag />}
+              aria-label="Basket"
+              onClick={onOpen}
+              className="basket-button"
+            />
+            {basket.length > 0 && (
+              <Badge
+                colorScheme="red"
+                borderRadius="full"
+                className="basket-badge"
+              >
+                {basket.length}
+              </Badge>
+            )}
           </li>
           <li><Link to="/cleng">Leng</Link></li>
         </ul>
