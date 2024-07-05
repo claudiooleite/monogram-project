f5703a4210ba537372f0bc42a678e4617d4b4a06
claudiooleite <claudio.leite@hotmail.com>
2024-07-05 10:17:44 +0200

    footer styled

--- src/pages/Shop-Product.jsx
+++ src/pages/Shop-Product.jsx
@@ -54,71 +54,74 @@ function ShopProduct() {
     return (
         <div className="parent-container">
             <Nav onOpen={onOpen} />
-            <div className="cssportal-grid">
-                <div className="div1 gridImagesMain">
-                    <div className="carousel-container">
-                        <div className="carousel-arrow left-arrow" onClick={handlePreviousImage}>
-                            <FontAwesomeIcon icon={faChevronLeft} />
-                        </div>
-                        <div className="gridImages1">
-                            <img src={product.images[currentImageIndex]} alt="" className="carousel-image" />
-                        </div>
-                        <div className="carousel-arrow right-arrow" onClick={handleNextImage}>
-                            <FontAwesomeIcon icon={faChevronRight} />
-                        </div>
-                    </div>
-                </div>
-                <div className="div2 flex-container">
-                    <div className="flex-items">
-                        <ProductCard product={product} handleAddToBasket={handleAddToBasket} />
-                    </div>
-                    <div className="flex-items"></div>
-                </div>
-                <div className="div3">
-                    <h2>MULTI-INSTRUMENT CONFIGURATOR APP</h2>
-                    <p>Included with every purchase is the Multi-Instrument Configurator app, a versatile and user-friendly application for macOS and Windows designed to optimize your musical instruments for any playing style, with no coding or scripting needed.</p>
-                    <ul>
-                        <li><strong>Customize Play Sensitivity and Range</strong>: Adjust the sensitivity and range of each instrument to achieve the perfect sound and feel.</li>
-                        <li><strong>Personalize LED Colors</strong>: Set and change the LED colors for any instrument control, enhancing your performance visually.</li>
-                        <li><strong>Advanced Control Gestures</strong>: Utilize advanced gestures for every input, including key press, key press & hold, and dynamic tilt mode.</li>
-                        <li><strong>Standard Input Device Mode</strong>: Map your instruments to standard input devices, such as keyboard shortcuts, macros, joystick, and mouse actions.</li>
-                        <li><strong>Digital Labeling and On-Screen Previews</strong>: Digitally label your instrument modules and bring up an on-screen preview to keep track of your settings effortlessly.</li>
-                        <li><strong>Expand Your Creativity</strong>: Unlock new possibilities with specialized packs for Audio, Photo, and Video editing, providing your instruments with hundreds of additional functions and profiles.</li>
-                    </ul>
-                </div>
-                <div className="div4">
-                    <div className="calltoaction">
-                        <div className="child-calltoaction">
-                            <h2>Everyday Essentials</h2>
-                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
-                            <p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
-                            <button>Learn More</button>
+            <div className="main-content">
+                <div className="cssportal-grid">
+                    <div className="div1 gridImagesMain">
+                        <div className="carousel-container">
+                            <div className="carousel-arrow left-arrow" onClick={handlePreviousImage}>
+                                <FontAwesomeIcon icon={faChevronLeft} />
+                            </div>
+                            <div className="gridImages1">
+                                <img src={product.images[currentImageIndex]} alt="" className="carousel-image" />
+                            </div>
+                            <div className="carousel-arrow right-arrow" onClick={handleNextImage}>
+                                <FontAwesomeIcon icon={faChevronRight} />
+                            </div>
                         </div>
                     </div>
-                </div>
-                <div className="div5">
-                    <h1>Also Recommended</h1>
-                    <div className="small-card-products">
-                        {musicItems.slice(0, 2).map((item, index) => (
-                            <Card
-                                key={item.name}
-                                item={item}
-                                index={index}
-                                handleHoverIn={handleHoverIn}
-                                handleHoverOut={handleHoverOut}
-                                hoveredIndex={hoveredIndex} />
-                        ))}
+                    <div className="div2 flex-container">
+                        <div className="flex-items">
+                            <ProductCard product={product} handleAddToBasket={handleAddToBasket} />
+                        </div>
+                        <div className="flex-items"></div>
                     </div>
-                </div>
-                <div className="div6">
-                    <div className="reviews-container">
-                        {customersSay.map((review, index) => (
-                            <ReviewCard key={index} review={review} />
-                        ))}
+                    <div className="div3">
+                        <h2>MULTI-INSTRUMENT CONFIGURATOR APP</h2>
+                        <p>Included with every purchase is the Multi-Instrument Configurator app, a versatile and user-friendly application for macOS and Windows designed to optimize your musical instruments for any playing style, with no coding or scripting needed.</p>
+                        <ul>
+                            <li><strong>Customize Play Sensitivity and Range</strong>: Adjust the sensitivity and range of each instrument to achieve the perfect sound and feel.</li>
+                            <li><strong>Personalize LED Colors</strong>: Set and change the LED colors for any instrument control, enhancing your performance visually.</li>
+                            <li><strong>Advanced Control Gestures</strong>: Utilize advanced gestures for every input, including key press, key press & hold, and dynamic tilt mode.</li>
+                            <li><strong>Standard Input Device Mode</strong>: Map your instruments to standard input devices, such as keyboard shortcuts, macros, joystick, and mouse actions.</li>
+                            <li><strong>Digital Labeling and On-Screen Previews</strong>: Digitally label your instrument modules and bring up an on-screen preview to keep track of your settings effortlessly.</li>
+                            <li><strong>Expand Your Creativity</strong>: Unlock new possibilities with specialized packs for Audio, Photo, and Video editing, providing your instruments with hundreds of additional functions and profiles.</li>
+                        </ul>
+                    </div>
+                    <div className="div4">
+                        <div className="calltoaction">
+                            <div className="child-calltoaction">
+                                <h2>Everyday Essentials</h2>
+                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                                <p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
+                                <button>Learn More</button>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="div5">
+                        <h1>Also Recommended</h1>
+                        <div className="small-card-products">
+                            {musicItems.slice(0, 2).map((item, index) => (
+                                <Card
+                                    key={item.name}
+                                    item={item}
+                                    index={index}
+                                    handleHoverIn={handleHoverIn}
+                                    handleHoverOut={handleHoverOut}
+                                    hoveredIndex={hoveredIndex} />
+                            ))}
+                        </div>
+                    </div>
+                    <div className="div6">
+                        <div className="reviews-container">
+                            {customersSay.map((review, index) => (
+                                <ReviewCard key={index} review={review} />
+                            ))}
+                        </div>
                     </div>
                 </div>
             </div>
             <BasketSummary isOpen={isOpen} onClose={onClose} />
+
             <Footer />
         </div>
     );
