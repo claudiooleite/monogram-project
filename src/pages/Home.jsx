202418191d419f3b8d0e1d9bb7ce2cf19f5940f7
claudiooleite <claudio.leite@hotmail.com>
2024-07-07 12:35:41 +0200

    shop link in home fixed

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,16 @@
 import React, { useEffect, useRef } from "react";
 import { useLocation } from "react-router-dom";
+import { useDisclosure } from "@chakra-ui/react";
 import Hero from "../components/home/Hero";
 import Products from "../components/home/Products";
 import Footer from "../components/common/Footer";
 import Nav from "../components/common/Nav";
+import BasketSummary from "../components/product/PreviewBasket";
 
 function Home() {
     const productsRef = useRef(null);
     const location = useLocation();
+    const { isOpen, onOpen, onClose } = useDisclosure();
 
     useEffect(() => {
         if (location.state?.scrollToProducts) {
@@ -17,11 +20,12 @@ function Home() {
 
     return (
         <>
-            <Nav productsRef={productsRef} />
+            <Nav productsRef={productsRef} onOpen={onOpen} />
             <Hero productsRef={productsRef} />
             <div className="parent-products" ref={productsRef}>
                 <Products/>
             </div>
+            <BasketSummary isOpen={isOpen} onClose={onClose} />
             <Footer />
         </>
     );
